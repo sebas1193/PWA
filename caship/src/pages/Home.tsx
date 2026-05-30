364321c239a27c7972ee504a9fbef935126af282
sebas1193 <juansebasitan05@gmail.com>
2026-05-30 15:30:00 -0500

    :tada: Start from scratch — clean Ionic/Capacitor base, no scaffolding!

---
+++ caship/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
