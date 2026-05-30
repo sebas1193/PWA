54b1d5d5eafd58145d1726d60bf8d10f4723db46
sebas1193 <juansebasitan05@gmail.com>
2026-05-30 15:30:00 -0500

    :wrench: chore(__vite.config.ts__): configure path alias @/ pointing to src/

--- caship/vite.config.ts
+++ caship/vite.config.ts
@@ -2,14 +2,22 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     legacy()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
